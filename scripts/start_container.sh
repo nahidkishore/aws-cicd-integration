e592b46a9a9173dd84297ecc94f839b3d50d0692
nahidkishore <nahidkishore99@gmail.com>
2026-04-09 13:58:14 +0600

    code changes

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,11 @@
 set -e
 
 # Pull the latest image
-docker pull "$DOCKER_REGISTRY_USERNAME/simple-python-flask-app:latest"
+# Note: Ensure your Docker Hub username is hardcoded here or fetched from SSM 
+# because CodeBuild environment variables don't persist to the EC2 instance.
+DOCKER_IMAGE="nahidkishore/simple-python-flask-app:latest"
+
+docker pull "$DOCKER_IMAGE"
 
 # Run the container
-docker run -d -p 80:5000 --name flask-app "$DOCKER_REGISTRY_USERNAME/simple-python-flask-app:latest"
+docker run -d -p 80:5000 --name flask-app "$DOCKER_IMAGE"
